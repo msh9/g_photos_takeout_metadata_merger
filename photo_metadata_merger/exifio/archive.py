13239771103fb3e8a27017c9097735f2f920dc58
Michael Hughes <michael@mihughes.com>
2023-05-09 13:14:51 -0600

    Simply contextmgr and iterable impl; working tests

--- photo_metadata_merger/exifio/archive.py
+++ photo_metadata_merger/exifio/archive.py
@@ -1,6 +1,5 @@
 import tarfile
 from pathlib import PurePath
-from contextlib import contextmanager
 
 _supported_image_file_extensions = [".jpg", ".jpeg", ".dng", ".png"]
 _supported_video_file_extensions = [".mkv", ".mp4"]
@@ -9,24 +8,23 @@ _json_file_suffix = '.json'
 class PhotoArchive:
     """PhotoArchive provides streaming methods for reading photos and metadata in pairs from Google Takeout Archives"""
 
-    def __init__(self, tarfile) -> None:
-        self.archive = tarfile
-        self.found_images = dict()
-        self.found_metadata = dict()
+    def __init__(self, tarfile_path) -> None:
+        self.tarfile_path = tarfile_path
+        self.archive_iterator = None
     
-    @contextmanager
-    def open(path):
-        """Opens the given tar archive and returns a generator of image, metadata tuples"""
+    def __enter__(self):
+        self.archive = tarfile.open(self.tarfile_path, 'r:gz')
+        return self
+        
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.archive.close()
+        return False
 
-        with tarfile.open(path, "r:gz") as archive:
-            pa = PhotoArchive(archive)
-            yield pa
-    
     def __iter__(self):
         return self
     
     def __next__(self):
-        return next(self._get_next_non_metadata_file())
+        return self._get_next_non_metadata_file()
 
     def _is_file_image_or_video(path):
         compressed_file_suffix = path.suffix
@@ -34,10 +32,18 @@ class PhotoArchive:
                     compressed_file_suffix in _supported_video_file_extensions)
 
     def _get_next_non_metadata_file(self):
-        for compressed_file in self.archive:
-            if compressed_file.isfile():
-                name_as_path = PurePath(compressed_file.name)
-                is_image_or_video = PhotoArchive._is_file_image_or_video(name_as_path)
-                if is_image_or_video:
-                    metadata_file_path = name_as_path.with_suffix(_json_file_suffix)
-                    yield (name_as_path, metadata_file_path)
+        if self.archive_iterator is None:
+            self.archive_iterator = iter(self.archive)
+
+        try:
+            while True:
+                compressed_file = next(self.archive_iterator)
+                if compressed_file.isfile():
+                    name_as_path = PurePath(compressed_file.name)
+                    is_image_or_video = PhotoArchive._is_file_image_or_video(name_as_path)
+                    if is_image_or_video:
+                        metadata_file_path = name_as_path.with_suffix(name_as_path.suffix + _json_file_suffix)
+                        return (name_as_path, metadata_file_path)
+        except StopIteration as ex:
+            self.archive_iterator = None
+            raise ex
--- tests/test_archive.py
+++ tests/test_archive.py
@@ -19,42 +19,41 @@ class TestPhotoArchive(unittest.TestCase):
         cls_path = pathlib.Path(__file__).parent.absolute()
         for resource_to_be_tarred in cls_path.joinpath(_resource_directory).iterdir():
             cls.archive.add(resource_to_be_tarred)
+        cls.archive.close()
     
     def test_photo_archive_context(self):
-        with archive.PhotoArchive.open(TestPhotoArchive.archive_path) as pa:
+        with archive.PhotoArchive(TestPhotoArchive.archive_path) as pa:
             self.assertIsInstance(pa, archive.PhotoArchive)
 
     def test_photo_archive_gets_next_image(self):
-        with archive.PhotoArchive.open(TestPhotoArchive.archive_path) as pa:
+        with archive.PhotoArchive(TestPhotoArchive.archive_path) as pa:
             non_metadata_path, metadata_path = next(pa)
             self.assertIsNotNone(non_metadata_path)
             self.assertIsNotNone(metadata_path)
-            self.assertFalse(non_metadata_path.endswith('.json'))
-            self.assertTrue(metadata_path.endswith('.json'))
+            self.assertFalse(non_metadata_path.name.endswith('.json'))
+            self.assertTrue(metadata_path.name.endswith('.json'))
 
     def test_photo_archive_returns_correct_metadata_per_image(self):
-        with archive.PhotoArchive.open(TestPhotoArchive.archive_path) as pa:
+        with archive.PhotoArchive(TestPhotoArchive.archive_path) as pa:
             non_metadata_archive_path, metadata_archive_path = next(pa)
             non_metadata_path = pathlib.PurePath(non_metadata_archive_path)
             self.assertEqual(pathlib.PurePath(metadata_archive_path), non_metadata_path.with_suffix(non_metadata_path.suffix + '.json')) 
 
     def test_photo_archive_returns_only_metadata_in_second_tuple_value(self):
-        with archive.PhotoArchive.open(TestPhotoArchive.archive_path) as pa:
+        with archive.PhotoArchive(TestPhotoArchive.archive_path) as pa:
             for _, metadata_archive_location in pa:
                 metadata_path = pathlib.PurePath(metadata_archive_location)
                 self.assertFalse(metadata_path.suffix != '.json')
 
     def test_photo_archive_only_returns_metadata_for_extant_images(self):
-        with archive.PhotoArchive.open(TestPhotoArchive.archive_path) as pa:
+        with archive.PhotoArchive(TestPhotoArchive.archive_path) as pa:
             for _, metadata_archive_location in pa:
                 metadata_path = pathlib.PurePath(metadata_archive_location)
-                self.failIf(metadata_path.name == 'other-img.jpg.json')
+                self.assertFalse(metadata_path.name == 'other-img.jpg.json')
 
     @classmethod
     def tearDownClass(cls):
-        cls.archive.close()
         cls._archive_directory.cleanup()
-
 
 if __name__ == '__main__':
     unittest.main()
