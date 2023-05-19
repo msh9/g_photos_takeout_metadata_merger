40afa1e5f6224f49f3f40862e130dcf2ec733337
Michael Hughes <michael@mihughes.com>
2023-05-19 08:50:00 -0600

    make metadata more idiomatic

--- photo_metadata_merger/exifio/metadata.py
+++ photo_metadata_merger/exifio/metadata.py
@@ -9,14 +9,25 @@ class TakeoutMetadata:
         self._metadata = json.loads(metadata)
 
     def get_creation_time(self) -> datetime.datetime:
-        if ('creationTime' in self._metadata 
-            and 'timestamp' in self._metadata['creationTime']):
-            return datetime.datetime.fromtimestamp(int(self._metadata['creationTime']['timestamp']), datetime.timezone.utc) 
-        else:
-            raise KeyError
+        return datetime.datetime.fromtimestamp(int(self._metadata['creationTime']['timestamp']), datetime.timezone.utc) 
 
-    def getPhotoTakenTime(self) -> datetime.datetime:
-        pass
+    def get_photo_taken_time(self) -> datetime.datetime:
+        return datetime.datetime.fromtimestamp(int(self._metadata['photoTakenTime']['timestamp']), datetime.timezone.utc)
+
+    def get_gphotos_location(self):
+        geoData = self._metadata['geoData']
+        return Location(geoData['latitude'], geoData['longitude'])
+
+    def get_exif_location(self):
+        exif_geo_data = self._metadata['geoDataExif']
+        return Location(exif_geo_data['latitude'], exif_geo_data['longitude'])
 
     def get_title(self) -> str:
-        pass
+        return self._metadata['title']
+
+class Location:
+    """Represents a physical location recorded by Google Photos"""
+
+    def __init__(self, latitude: float, longitude: float):
+        self.latitude = latitude
+        self.longitude = longitude
