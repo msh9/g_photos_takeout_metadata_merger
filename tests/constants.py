1db80369199a4440bf851eab1bd629809500821c
Michael Hughes <michael@mihughes.com>
2023-05-23 14:02:40 -0600

    Content module tests; human updated from initial gpt4 written tests

--- tests/constants.py
+++ tests/constants.py
@@ -5,6 +5,7 @@ tarone_resource_directory = resource_directory + '/tarone'
 tartwo_resource_directory = resource_directory + '/tartwo'
 image_metadata_filename = tarone_resource_directory + '/example-img.png.json'
 video_metadata_filename = tartwo_resource_directory + '/example-video.mp4.json'
+image_filename = tarone_resource_directory + '/example-img.png'
 
 def get_tests_folder() -> PurePath:
     return Path(__file__).parent.absolute()
@@ -18,3 +19,6 @@ def get_video_metadata() -> str:
     with open(get_tests_folder().joinpath(video_metadata_filename), 'r') as metadata:
         lines = metadata.read()
     return lines
+
+def get_exif_fixture_path() -> PurePath:
+    return get_tests_folder().joinpath(resource_directory).joinpath('exif-fixture.jpg')
--- tests/test_content.py
+++ tests/test_content.py
@@ -1,58 +1,73 @@
 import unittest
 import pathlib
 import exif
-import os
 import tempfile
-from content import JpgContent
-from metadata import TakeoutMetadata, Location
+from photo_metadata_merger.exifio.content import JpgContent
+from photo_metadata_merger.exifio.metadata import TakeoutMetadata
 import constants
+import json
 
 class TestJpgContent(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        # Create a temporary directory for saving images
-        cls.temp_dir = tempfile.TemporaryDirectory()
+        cls.test_output_directory = tempfile.TemporaryDirectory()
+        content_path = constants.get_exif_fixture_path()
+        with open(content_path, 'rb') as image_file:
+            image = image_file.read()
+
+        mock_metadata_dict = {
+            "creationTime": {"timestamp": "1684784093"},
+            "photoTakenTime": {"timestamp": "1621573200"},
+            "geoData": {"latitude": 45.4215, "longitude": -75.6972},
+            "geoDataExif": {"latitude": 45.4215, "longitude": -75.8972},
+            "title": "test_photo"
+        }
+
+        cls.test_file_path = pathlib.Path(cls.test_output_directory.name, "test.jpg")
+        metadata = TakeoutMetadata(json.dumps(mock_metadata_dict))
+
+        jpg_content = JpgContent(image, metadata)
+        jpg_content.process_content_metadata(cls.test_file_path)
+
+        with open(cls.test_file_path, 'rb') as fixture_image:
+            cls.test_fixture = fixture_image.read()
+
+    def test_process_content_exists(self):
+        self.assertTrue(TestJpgContent.test_file_path.exists())
+
+    def test_process_content_updates_timestamps(self):
+        exif_data = exif.Image(TestJpgContent.test_fixture)
+
+        self.assertEqual(exif_data.get('datatime_original'), '2021-05-21 00:00')
+        self.assertEqual(exif_data.get('datatime_digitized'), '2023-05-22 13:34:53')
+
+    def test_process_content_updates_location(self):
+        exif_data = exif.Image(TestJpgContent.test_fixture)
+
+        latitude_degrees_minutes_seconds = exif_data.get('gps_latitude')
+        longitude_degrees_minutes_seconds = exif_data.get('gps_longitude')
+
+        self.assertAlmostEqual(45.4215, TestJpgContent._deg_minute_second_to_decimal(*latitude_degrees_minutes_seconds))
+        self.assertAlmostEqual(75.8972, TestJpgContent._deg_minute_second_to_decimal(*longitude_degrees_minutes_seconds))
+
+        self.assertEqual(exif_data.get('gps_latitude_ref'), 'N')
+        self.assertEqual(exif_data.get('gps_longitude_ref'), 'W')
+
+    def test_process_content_updates_title(self):
+        exif_data = exif_data(TestJpgContent.test_fixture)
+
+        self.assertEqual(exif_data.get('xp_title'), 'test_photo')
+
+    @staticmethod
+    def _deg_minute_second_to_decimal(degrees, minutes, seconds) -> float:
+        return (degrees +
+                minutes / 60 +
+                seconds / 60)
 
     @classmethod
     def tearDownClass(cls):
-        # Clean up the temporary directory after all tests are done
-        cls.temp_dir.cleanup()
-
-    def setUp(self):
-        # Prepare a sample JpgContent instance for use in tests.
-        # This uses an actual image file and mock metadata.
-        content_path = constants.get_image_path()
-        with open(content_path, 'rb') as f:
-            content = f.read()
-
-        mock_metadata_dict = {
-            "creationTime": {"timestamp": "1621573200"},
-            "photoTakenTime": {"timestamp": "1621573200"},
-            "geoData": {"latitude": 45.4215, "longitude": -75.6972},
-            "geoDataExif": {"latitude": 45.4215, "longitude": -75.6972},
-            "title": "test_photo"
-        }
-        metadata = TakeoutMetadata(mock_metadata_dict)
-        self.jpg_content = JpgContent(content, metadata)
-
-    def test_update_content_metadata_datetime_original(self):
-        # Update metadata
-        updated_image = self.jpg_content._update_content_metadata()
-
-        # Check the datetime_original field
-        self.assertEqual(updated_image.datetime_original, "2021-05-21 00:00")
-
-    def test_save_content(self):
-        # Define the path to save the content to
-        save_to_path = pathlib.PurePath(self.temp_dir.name, "test.jpg")
-
-        # Update metadata and save the content
-        updated_image = self.jpg_content._update_content_metadata()
-        self.jpg_content._save_content(updated_image, save_to_path)
-
-        # Check that the file was saved successfully
-        self.assertTrue(os.path.isfile(save_to_path))
+        cls.test_output_directory.cleanup()
 
 if __name__ == "__main__":
     unittest.main()
